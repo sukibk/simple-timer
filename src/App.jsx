132ba0f14f6a4892cc64d00a0ba1ce73209c7c91
Marko Sudar <mjsudar21@catawba.edu>
2023-04-18 00:19:45 -0400

    Timer

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import {useRef, useState} from 'react';
+
+
+export default function App() {
+
+  const [currentTime, setCurrentTime] = useState(null)
+  const [startTime, setStartTime] = useState(null);
+  let timerId = useRef();
+
+  function handleStartTimer() {
+    setStartTime(Date.now());
+    setCurrentTime(Date.now());
+
+    timerId.current = setInterval(() => {
+      setCurrentTime(Date.now())
+    }, 10)
+  }
+
+  function handleCancelTimer() {
+    clearInterval(timerId.current)
+  }
+
+  let secondsPassed = (currentTime - startTime) / 1000;
+
+  return (
+      <div className='timer'>
+        <div className={'text'}>
+        <h1>Timer: {secondsPassed}</h1>
+        </div>
+        <div className='actions'>
+        <button onClick={handleStartTimer}>Start</button>
+        <button onClick={handleCancelTimer}>Stop</button></div>
+    </div>
+  );
+}
